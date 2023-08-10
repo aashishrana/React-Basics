66d655ab5dbd62da1fb67b64bb2e6d7ce9ceaed0
aashishrana <aashishrana71135@gmail.com>
2023-08-10 00:47:10 -0700

    A new Avatar component in which data passing through props

---
+++ src/component/Avatar.jsx
@@ -0,0 +1,13 @@
+function Avatar({src , width , height, children}) {
+    return (
+        <>
+
+        <img src={src} width={width} height={height} />
+        {children}
+        
+        </>
+    )
+
+}
+
+export default Avatar;
